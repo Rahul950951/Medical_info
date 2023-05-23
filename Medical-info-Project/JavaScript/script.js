15f22767006a00fd151e7417e65e6d24b7619e5e
Rahul kumar kapar <97392109+Rahul950951@users.noreply.github.com>
2023-05-24 01:19:34 +0530

    Add files via upload

---
+++ Medical-info-Project/JavaScript/script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
